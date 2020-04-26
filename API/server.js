03e1e5e0f4f0df6ea037b18976e887702252ac17
Siddhartha Mishra <siddhartham04@gmail.com>
2020-04-26 12:26:14 -0700

    created dbConnect class

--- API/server.js
+++ API/server.js
@@ -1,16 +1,11 @@
 var express = require("express"); 
 var app = express(); 
-var admin = require('firebase-admin');
-var serviceAccount = require("/Users/siddharthamishra/Documents/GitHub/CovidHelper/API/covidhelper-45bda-firebase-adminsdk-4ssgh-dfd30a3fc6.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://covidhelper-45bda.firebaseio.com"
-});
+let db = require("./dbConnect.js"); 
+db.setUpDB(); 
+var data = db.getDb(); 
 
-let db = admin.firestore(); 
-
-
+db.getVolunteers(data); 
 
 app.get("/", function(req, res){
 
