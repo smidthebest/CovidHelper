55a6a2a959462e16bf7fade2ed16861ecea98f84
Arnav Reddy <arnavreddy@gmail.com>
2020-04-25 17:31:31 -0700

    init project

---
+++ IOS/CovidHelper/CovidHelper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CovidHelper
+//
+//  Created by Arnav Reddy on 4/25/20.
+//  Copyright © 2020 Arnav Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
